95bdb291abd67cb1e03583f31d476d7eb0899296
Bogdanchikov-Ilya <test200373@gmail.com>
2021-10-10 17:45:25 +0300

    fix box sizing

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,9 @@
 document.querySelector('.burger-menu').addEventListener('click', (e) => {
   document.querySelector('.burger-menu').classList.toggle('active')
+  document.querySelector('.header-content').classList.toggle('active')
+  if(document.querySelector('.burger-menu').classList.contains('active')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'auto';
+  }
 })
